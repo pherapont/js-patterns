4090741993d3a62bbe1d09f9f0a82cdef33a993a
pherapont <pherapont@yandex.ru>
2020-10-15 06:42:34 +0700

    chain of responsibitlity

---
+++ 3 behavior/10_chain_of_responsibility.js
@@ -0,0 +1,20 @@
+//  Поведенчиские паттерны - для улучшения коомуникации между объектами разного типа
+//  Chain of responsibility позволяет последовательно вызывать у объекта набор операций и последовательно модифицировать 
+//  Например jQuery
+
+class MySum {
+	constructor(initialValue = 42) {
+		this.sum = initialValue
+	}
+
+	add(value) {
+		this.sum += value
+		return this
+	}
+}
+
+const sum1 = new MySum()
+console.log(sum1.add(8).add(10).add(1).sum)
+
+const sum2 = new MySum(0)
+console.log(sum2.add(1).add(2).sum)
